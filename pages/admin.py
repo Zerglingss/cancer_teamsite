23b023d903294d9d5c122e82d1b53ac5230eb6bb
ZerglingSS <team.cancersite@gmail.com>
2025-04-25 20:58:00 -0400

    broke everything

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import ContactMessage
 
-admin.site.register(ContactMessage)
+from django.core.mail import send_mail
+
+
+@admin.action(description="Send welcome email")
+def send_welcome_email(modeladmin, request, queryset):
+    for customer in queryset:
+        send_mail(
+            subject="Welcome!",
+            message="Don't piss off Admiral Jinx.",
+            from_email="cancer.teamsite@example.com",
+            recipient_list=["cancer.teamsite@gmail.com"],
+            fail_silently=False,
+        )
+
+class CustomerAdmin(admin.ModelAdmin):
+    actions = [send_welcome_email]
+
+admin.site.register(ContactMessage, CustomerAdmin)
